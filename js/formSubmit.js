ff64bfcd26bf564e764e14aea709f22d1b1102c0
PNT1AGrupo3 <grupo3pnt@gmail.com>
2022-09-14 08:15:30 -0300

    Js Submit Form
    Controla que el nombre y el apellido no contengan números espacios o caracteres especiales

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,3 +1,14 @@
-function submitClick(){
-    alert("hola");
+function submitClick() {
+    var resultado = false;
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById('apellido').value;
+    if (soloLetras(nombre) && soloLetras(apellido)) {
+        resultado = true;
+    } else {
+        alert("Nombre y Apellido Incorrecto! No incluya espacios, números o caracteres especiales");
+    }
+    return resultado;
+}
+function soloLetras(texto) {
+    return /^[a-zA-Z]+$/.test(texto);
 }
